53d60b8f11f9424471eda181bf9d9646de7c8a9b
jinho <rowkaxl@naver.com>
2022-03-03 14:29:19 +0900

    initial

---
+++ PolishingApp/PolishingApp/PolishingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolishingAppApp.swift
+//  PolishingApp
+//
+//  Created by jinho on 2022/03/03.
+//
+
+import SwiftUI
+
+@main
+struct PolishingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
